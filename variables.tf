3f5eb20ddfbdebac017a4905a7634f3396737cd3
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-04-03 15:51:47 +0200

    Add a product-scoped Key Vault

--- variables.tf
+++ variables.tf
@@ -30,3 +30,11 @@ variable "application_type" {
   default     = "Web"
   description = "Type of Application Insights (Web/Other)"
 }
+
+variable "tenant_id" {
+  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
+}
+
+variable "jenkins_AAD_objectId" {
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
