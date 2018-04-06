719f9798da4d9a61608c3793c7753c99cca2e65d
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-04-06 14:47:00 +0100

    Remove 'appinsights' from the name

--- application-insights.tf
+++ application-insights.tf
@@ -1,5 +1,5 @@
 resource "azurerm_application_insights" "appinsights" {
-  name                = "${var.product}-appinsights-${var.env}"
+  name                = "${var.product}-${var.env}"
   location            = "${var.appinsights_location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   application_type    = "${var.application_type}"
