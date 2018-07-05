4a00c1bf258c7b3249cd6b656d8fdbb3fbe8aaf7
andrewwa-kainos <andrewwa@kainos.com>
2018-07-05 11:55:32 +0100

    don't create a new resource group

--- azure-bulk-print-fail-alerts.tf
+++ azure-bulk-print-fail-alerts.tf
@@ -2,7 +2,7 @@ module "cmc-bulk-print-failures" {
   source = "git@github.com:andrewwa-kainos/moj-module-action-group"
   location = "${var.location}"
   env = "${var.env}"
-  resourcegroup_name = "${azurerm_resource_group.rg.name}-bpf"
+  resourcegroup_name = "${azurerm_resource_group.rg.name}"
   group_name = "BPF_${var.env}"
   alert_name = "Bulk Print Fail Alert - ${var.env}"
   alert_short_name = "bpfprinterr"
