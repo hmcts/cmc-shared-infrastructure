421b1437152a5a093f3783666557163c8d8219ea
andrewwa-kainos <andrewwa@kainos.com>
2018-07-06 10:22:28 +0100

    prepared for prod

---
+++ bulk-print-fail-action-group.tf
@@ -0,0 +1,12 @@
+module "cmc-bulk-print-fail-action-group" {
+  source = "git@github.com:hmcts/moj-module-action-group"
+  location = "${var.location}"
+  env = "${var.env}"
+  subscription = "${var.env == "prod" ? "DCD-CNP-Prod" : "DCD-CFT-Sandbox"}"
+  resourcegroup_name = "${azurerm_resource_group.rg.name}"
+  group_name = "BPF_${var.env}"
+  alert_name = "Bulk Print Fail Alert - ${var.env}"
+  alert_short_name = "BPF_alert"
+  email_receiver_name = "Bulk Print Alerts"
+  email_receiver_address = "cmc-bulk-print-alerts@hmcts.net"
+}
