b3a7069dd548f91fa1d0b3dd379f261c91830560
andrewwa-kainos <andrewwa@kainos.com>
2018-07-11 11:06:02 +0100

    Fix variable names and direct to module-action-group branch with no subscription variable

--- bulk-print-fail-action-group.tf
+++ bulk-print-fail-action-group.tf
@@ -1,10 +1,10 @@
 module "cmc-bulk-print-fail-action-group" {
-  source = "git@github.com:hmcts/cnp-module-action-group"
+  source = "git@github.com:hmcts/cnp-module-action-group?ref=feature/ROC-3821"
   location = "${var.location}"
   env = "${var.env}"
   resourcegroup_name = "${azurerm_resource_group.rg.name}"
   action_group_name = "Bulk Print Fail Alert - ${var.env}"
-  alert_short_name = "BPF_alert"
+  short_name = "BPF_alert"
   email_receiver_name = "Bulk Print Alerts"
 //  email_receiver_address = "cmc-bulk-print-alerts@hmcts.net" - disable for testing
   email_receiver_address = "andrew.walker@hmcts.net"
