ca7b452e7fe469b99b99e514a1a7021ecadd5f8c
andrewwa-kainos <andrewwa@kainos.com>
2018-07-12 15:27:06 +0100

    Prepare for prod

--- bulk-print-fail-action-group.tf
+++ bulk-print-fail-action-group.tf
@@ -1,12 +1,11 @@
 module "cmc-bulk-print-fail-action-group" {
-  source = "git@github.com:hmcts/cnp-module-action-group?ref=feature/ROC-3821_with-output"
-  location = "global"
+  source = "git@github.com:hmcts/cnp-module-action-group"
+  location = "${var.location}"
   env = "${var.env}"
 
   resourcegroup_name = "${azurerm_resource_group.rg.name}"
   action_group_name = "Bulk Print Fail Alert - ${var.env}"
   short_name = "BPF_alert"
   email_receiver_name = "Bulk Print Alerts"
-//  email_receiver_address = "cmc-bulk-print-alerts@hmcts.net" disable for testing
-  email_receiver_address = "andrew.walker@hmcts.net"
+  email_receiver_address = "cmc-bulk-print-alerts@hmcts.net"
 }
