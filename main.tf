99a1d445111bf89c80e6ca0ddd784c0f504e1217
Tim Jacomb <tim.jacomb@hmcts.net>
2018-08-15 21:48:16 +0100

    Fix RG name
    This reverts commit 3c8b049c211b8b92d836261366f7bde1dc22f158.

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 resource "azurerm_resource_group" "rg" {
-  // TODO change this back to product-env
-  name     = "${var.product}-${var.env}-shared-infrastructure"
+  name     = "${var.product}-${var.env}"
   location = "${var.location}"
 }
