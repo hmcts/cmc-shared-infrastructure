4e2e25b7c506f477184f337a8884b4766996b341
Mark Ryan <mark.ryan@hmcts.net>
2018-11-22 20:17:30 +0000

    dropping changes to test

--- shutter-page.tf
+++ shutter-page.tf
@@ -1,11 +1,11 @@
-module "shutterPage" {
-  source = "git@github.com:hmcts/moj-module-shutterpage?ref=CNP-585"
-  location = "${var.location}"
-  env = "${var.env}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  tag_list = "${local.tags}"
-  product = "${var.product}"
-  shutterCustomDomain = "${var.citizen_shutter_domain}"
-  shutterPageDirectory = "defaultShutterPageCitizen"
-  subscription = "${var.subscription}"
-}
+# module "shutterPage" {
+#   source = "git@github.com:hmcts/moj-module-shutterpage?ref=CNP-585"
+#   location = "${var.location}"
+#   env = "${var.env}"
+#   resource_group_name = "${azurerm_resource_group.rg.name}"
+#   tag_list = "${local.tags}"
+#   product = "${var.product}"
+#   shutterCustomDomain = "${var.citizen_shutter_domain}"
+#   shutterPageDirectory = "defaultShutterPageCitizen"
+#   subscription = "${var.subscription}"
+# }
