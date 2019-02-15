c08faf981e52eb5c8a17f45232e0bac046819e19
Mark Ryan <mark.ryan@hmcts.net>
2019-02-15 12:15:09 +0000

    finger in air

--- alerts.tf
+++ alerts.tf
@@ -1,3 +1,21 @@
+module "cmc-doc-mgt-fail-alert" {
+  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  location          = "${azurerm_application_insights.appinsights.location}"
+  app_insights_name = "${azurerm_application_insights.appinsights.name}"
+
+  alert_name                 = "Document Management failure - CMC"
+  alert_desc                 = "Triggers when a Document Management upload or download failure event is received from CMC in a 5 minute poll."
+  app_insights_query         = "customEvents | where name == \"ff4J cmc_admissions failure\""
+  frequency_in_minutes       = 5
+  time_window_in_minutes     = 5
+  severity_level             = "3"
+  action_group_name          = "${module.cmc-doc-mgt-failure-action-group.action_group_name}"
+  custom_email_subject       = "CMC Document Management Failure"
+  trigger_threshold_operator = "GreaterThan"
+  trigger_threshold          = 0
+  resourcegroup_name         = "${azurerm_resource_group.rg.name}"
+}
+
 module "cmc-bulk-print-fail-alert" {
   source            = "git@github.com:hmcts/cnp-module-metric-alert"
   location          = "${azurerm_application_insights.appinsights.location}"
@@ -57,27 +75,6 @@ module "cmc-ff4j-admissions-fail-alert" {
   severity_level             = "3"
   action_group_name          = "${module.cmc-ff4j-admissions-fail-action-group.action_group_name}"
   custom_email_subject       = "CMC FF4J CMC_Admissions Failure"
-  trigger_threshold_operator = "GreaterThan"
-  trigger_threshold          = 0
-  resourcegroup_name         = "${azurerm_resource_group.rg.name}"
-}
-
-module "cmc-doc-mgt-fail-alert" {
-  source            = "git@github.com:hmcts/cnp-module-metric-alert"
-  location          = "${azurerm_application_insights.appinsights.location}"
-  app_insights_name = "${azurerm_application_insights.appinsights.name}"
-
-  alert_name                 = "Document Management failure - CMC"
-  alert_desc                 = "Triggers when a Document Management upload or download failure event is received from CMC in a 5 minute poll."
-  app_insights_query = <<AIQ
-customEvents
-| where name == "Document management upload - failure" or name == "Document management download - failure"
-AIQ
-  frequency_in_minutes       = 5
-  time_window_in_minutes     = 5
-  severity_level             = "3"
-  action_group_name          = "${module.cmc-doc-mgt-failure-action-group.action_group_name}"
-  custom_email_subject       = "CMC Document Management Failure"
   trigger_threshold_operator = "GreaterThan"
   trigger_threshold          = 0
   resourcegroup_name         = "${azurerm_resource_group.rg.name}"
