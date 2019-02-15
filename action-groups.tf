df48651dada4c57995c553eb30b8dd155522192b
Mark Ryan <mark.ryan@hmcts.net>
2019-02-15 18:42:44 +0000

    rm secrets

--- action-groups.tf
+++ action-groups.tf
@@ -1,10 +1,4 @@
 // Bulk print failures
-
-data "azurerm_key_vault_secret" "bpf_email_secret" {
-  name      = "bulk-print-failure-email"
-  vault_uri = "${data.azurerm_key_vault.cmc_key_vault.vault_uri}"
-}
-
 module "cmc-bulk-print-fail-action-group" {
   source   = "git@github.com:hmcts/cnp-module-action-group"
   location = "global"
@@ -14,7 +8,7 @@ module "cmc-bulk-print-fail-action-group" {
   action_group_name      = "Bulk Print Fail Alert - ${var.env}"
   short_name             = "BPF_alert"
   email_receiver_name    = "Bulk Print Alerts"
-  email_receiver_address = "${data.azurerm_key_vault_secret.bpf_email_secret.value}"
+  email_receiver_address = "bulk-print-failure@email.com"
 }
 
 output "bpf_action_group_name" {
@@ -22,12 +16,6 @@ output "bpf_action_group_name" {
 }
 
 // PDF service failures
-
-data "azurerm_key_vault_secret" "pdf_fail_email_secret" {
-  name      = "pdf-failure-email"
-  vault_uri = "${data.azurerm_key_vault.cmc_key_vault.vault_uri}"
-}
-
 module "cmc-pdf-fail-action-group" {
   source   = "git@github.com:hmcts/cnp-module-action-group"
   location = "global"
@@ -37,7 +25,7 @@ module "cmc-pdf-fail-action-group" {
   action_group_name      = "PDF Failure Alert - ${var.env}"
   short_name             = "PDF_alert"
   email_receiver_name    = "PDF Failure Alerts"
-  email_receiver_address = "${data.azurerm_key_vault_secret.pdf_fail_email_secret.value}"
+  email_receiver_address = "pdf-failure@email.com"
 }
 
 output "pdf_action_group_name" {
@@ -45,12 +33,6 @@ output "pdf_action_group_name" {
 }
 
 // CMC Admission Failures
-
-data "azurerm_key_vault_secret" "ff4j_email_secret" {
-  name      = "ff4j-admissions-failure-email"
-  vault_uri = "${data.azurerm_key_vault.cmc_key_vault.vault_uri}"
-}
-
 module "cmc-ff4j-admissions-fail-action-group" {
   source   = "git@github.com:hmcts/cnp-module-action-group"
   location = "global"
@@ -60,7 +42,7 @@ module "cmc-ff4j-admissions-fail-action-group" {
   action_group_name      = "ff4j Admissions Failure Alert - ${var.env}"
   short_name             = "ff4j_alert"
   email_receiver_name    = "ff4j Admissions Failure Alerts"
-  email_receiver_address = "${data.azurerm_key_vault_secret.ff4j_email_secret.value}"
+  email_receiver_address = "ff4j-admissions-failure@email.com"
 }
 
 output "ff4j_failure_action_group_name" {
@@ -68,12 +50,6 @@ output "ff4j_failure_action_group_name" {
 }
 
 // CMC Document Management Failure
-
-data "azurerm_key_vault_secret" "doc_mgt_email_secret" {
-  name      = "document-management-failure-email"
-  vault_uri = "${data.azurerm_key_vault.cmc_key_vault.vault_uri}"
-}
-
 module "cmc-doc-mgt-failure-action-group" {
   source   = "git@github.com:hmcts/cnp-module-action-group"
   location = "global"
@@ -83,7 +59,7 @@ module "cmc-doc-mgt-failure-action-group" {
   action_group_name      = "Document Management Failure Alert - ${var.env}"
   short_name             = "DM_alert"
   email_receiver_name    = "Document Management Failure Alerts"
-  email_receiver_address = "${data.azurerm_key_vault_secret.doc_mgt_email_secret.value}"
+  email_receiver_address = "doc-mgt-failure@email.com"
 }
 output "doc_mgmt_failure_action_group_name" {
   value = "${module.cmc-doc-mgt-failure-action-group.action_group_name}"
