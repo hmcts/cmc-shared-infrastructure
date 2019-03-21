44276554dfcd603ad2e828f9dce2bbf9f209932c
Tim Jacomb <t.jacomb@kainos.com>
2019-03-21 16:42:22 +0000

    Fix common tags missing on vault (#31)

--- key-vault.tf
+++ key-vault.tf
@@ -7,6 +7,7 @@ module "cmc-vault" {
   object_id               = "${var.jenkins_AAD_objectId}"
   resource_group_name     = "${azurerm_resource_group.rg.name}"
   product_group_object_id = "68839600-92da-4862-bb24-1259814d1384"
+  common_tags             = "${local.tags}"
 }
 
 data "azurerm_key_vault" "cmc_key_vault" {
