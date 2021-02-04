8029e2cd8c01edb5ffa7a84434a6c9ffa70546fb
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-02-04 14:01:26 +0000

    Update application-insights.tf

--- application-insights.tf
+++ application-insights.tf
@@ -3,6 +3,14 @@ resource "azurerm_application_insights" "appinsights" {
   location            = "${var.appinsights_location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   application_type    = "${var.application_type}"
+  
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
+      # destroys and re-creates this appinsights instance
+      application_type,
+    ]
+  }
 }
 
 resource "azurerm_key_vault_secret" "appInsights-InstrumentationKey" {
