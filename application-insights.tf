a3fa47d230c7e026bae91053bb6097e13b86165e
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-01-03 10:15:09 +0000

    removing azurerm resouce

--- application-insights.tf
+++ application-insights.tf
@@ -1,18 +1,3 @@
-resource "azurerm_application_insights" "appinsights" {
-  name                = "${var.product}-${var.env}"
-  location            = var.appinsights_location
-  resource_group_name = azurerm_resource_group.rg.name
-  application_type    = var.application_type
-
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
-      # destroys and re-creates this appinsights instance
-      application_type,
-    ]
-  }
-}
-
 module "application_insights" {
   source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
 
