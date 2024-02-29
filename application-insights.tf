ede1ca8b3a2fbc3e6a8b3e9887835acb4128c188
Linus Norton <linusnorton@gmail.com>
2024-02-29 10:00:37 +0000

    Add app insights connection string

--- application-insights.tf
+++ application-insights.tf
@@ -19,3 +19,9 @@ resource "azurerm_key_vault_secret" "appInsights-InstrumentationKey" {
   value        = module.application_insights.instrumentation_key
   key_vault_id = module.cmc-vault.key_vault_id
 }
+
+resource "azurerm_key_vault_secret" "app_insights_connection_string" {
+  name         = "app-insights-connection-string"
+  value        = module.application_insights.connection_string
+  key_vault_id = module.cmc-vault.key_vault_id
+}
