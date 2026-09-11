e58fec75810ad5ce4fa4f2150d5f7ac7a4094cb7
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-11 15:54:02 +0100

    DTSPO-34656: update key vault module branch

--- key-vault.tf
+++ key-vault.tf
@@ -4,7 +4,7 @@ data "azurerm_user_assigned_identity" "jenkins" {
 }
 
 module "cmc-vault" {
-  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
   name                    = "cmc-${var.env}"
   product                 = var.product
   env                     = var.env
